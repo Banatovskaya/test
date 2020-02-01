de2f3d231cd370ec3b9665eef92d357c258c01e9
Irina <banatovskaya#gmail.com>
2020-02-01 18:45:13 +0200

    4commit

--- 1.js
+++ 1.js
@@ -1,4 +1,53 @@
 "use strict";
+let start = document.querySelector("#start");
+
+//Получить все блоки в правой части программы через классы (которые имеют класс название-value, начиная с <div class="budget-value"></div> и заканчивая <div class="yearsavings-value"></div>)
+let budgetValue = document.querySelector (".budget-value"),
+    dayBudgetValue = document.querySelector (".daybudget-value"),
+    levelValue = document.querySelector (".level-value"),
+    expensesValue = document.querySelector (".expenses-value"),
+    incomeValue = document.querySelector (".income-value"),
+    monthSavingsValue = document.querySelector (".monthsavings-value"),
+    yearsSvingsValue = document.querySelector (".yearsavings-value");
+  
+//Получить поля(input) c обязательными расходами через класс. (class=”expenses-item”)
+let name1 = document.querySelectorAll (".expenses-item")[0],
+    price1 = document.querySelectorAll (".expenses-item")[1],
+    name2 = document.querySelectorAll (".expenses-item")[2],
+    price2 = document.querySelectorAll (".expenses-item")[3];
+   
+// Получить кнопки “Утвердить” и “Рассчитать” через Tag, каждую в своей переменной. 
+let approveExpenses = document.querySelectorAll ("button")[0],   
+    approveOptionalExpenses = document.querySelectorAll ("button")[1],
+    countDayBudget = document.querySelectorAll ("button")[2];
+
+// Получить поля для ввода необязательных расходов (optionalexpenses-item) при помощи querySelectorAll
+let optionalExpenses1 = document.querySelectorAll(".optionalexpenses-item")[0],
+    optionalExpenses2 = document.querySelectorAll(".optionalexpenses-item")[1],
+    optionalExpenses3 = document.querySelectorAll(".optionalexpenses-item")[2];
+
+// Получить оставшиеся поля через querySelector (статьи возможного дохода, чекбокс, сумма, процент,
+// год, месяц, день)*/
+let possibleIncome = document.querySelector("#income"),
+    sevingCheckbox = document.querySelector("#seving"),
+    sevingSum = document.querySelector("#sum"),
+    sevingPersent = document.querySelector("persent"),
+    yearValue = document.querySelector (".year-value"),
+    monthValue = document.querySelector (".month-value"),
+    dayValue = document.querySelector (".day-value");
+
+start.addEventListener("onclick". function(){
+    time = prompt("Введите дату в формате YYYY-MM-DD", ""); 
+    money = +prompt("Ваш бюджет на месяц?", "");
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+    
+});
+
+   
+
+
 var a, b;
 var money, time;
 let appData = {
@@ -71,14 +120,7 @@ console.log(appData);
 appData.checkSevings();
 appData.chooseOptExpenses();
 
-function start() {
-    money = +prompt("Ваш бюджет на месяц?", "");
-    time = prompt("Введите дату в формате YYYY-MM-DD", ""); 
-    while (isNaN(money) || money == "" || money == null) {
-        money = +prompt("Ваш бюджет на месяц?", "");
-    }
-    return money;
-};
+
 console.log("Наша программа включает в себя данные:");
 for (key in appData){
     console.log(key, appData[key]);
